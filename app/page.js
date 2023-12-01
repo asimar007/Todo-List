be9ed8598d6435a018e1b34a2b8ecd5debe3ed2f
Shyamal Das <shyamald934@gmail.com>
2023-12-01 17:11:20 +0530

    Functionality changed, Local Storage added and style changes done

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const page = () => {
   const [title, settitle] = useState("")
@@ -14,19 +14,43 @@ const page = () => {
 
   const submithlr = (e) => {
     e.preventDefault()
+    if(title.length>0 && desc.length>0){
     setMainTask([...MainTask, { title, desc }])
     settitle("")
     setdesc("")
+    }
+    else{
+      if(title.length==0 && desc.length==0)
+      alert("Please enter Title & Description")
+      else if(title.length==0)
+      alert("Please enter Title")
+      else
+      alert("Please enter Description")
+    }
   }
+
+  useEffect(()=>{
+    // String to JSON
+    const MainTask=JSON.parse(localStorage.getItem("MainTask"))
+    if(MainTask && MainTask.length>0)
+    {
+      setMainTask(MainTask);
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("MainTask", JSON.stringify(MainTask))
+  },[MainTask])
+
   // ! Main Task Code
-  let renderTask = <h2 className='text-2xl font-semibold '>No Task Available</h2>
+  let renderTask = <h2 className='text-2xl font-semibold text-center'>No Task Available</h2>
 
   if (MainTask.length > 0) {
     renderTask = MainTask.map((t, i) => {
       return (
 
-        <li key={i} className='flex item-center justify-between'>
-          <div className='flex item-center justify-between mb-5 w-2/3'>
+        <li key={i} className='flex item-center justify-between mt-2 bg-slate-200'>
+          <div className='flex item-center justify-between mb-2 w-2/3 mx-4 mt-4'>
             <h5 className='text-2xl font-semibold'>{t.title}</h5>
             <h6 className='text-2xl font-semibold'>{t.desc}</h6>
           </div>
@@ -34,7 +58,7 @@ const page = () => {
             onClick={() => {
               deletehlr(i)
             }}
-            className='bg-red-600 rounded px-3 py-2 text-white mb-5'>Delete</button>
+            className='bg-red-600 rounded px-3 py-2 text-white mb-5 mt-4 mr-4'>Delete</button>
         </li>
       )
     })
@@ -43,7 +67,7 @@ const page = () => {
   return (
     <>
       <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Asim Todo List</h1>
-      <form onSubmit={submithlr}>
+      <form onSubmit={submithlr} className='text-center'>
         <input type="text" className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter Your Task'
           value={title}
           onChange={(e) => {
@@ -54,10 +78,10 @@ const page = () => {
           onChange={(e) => {
             setdesc(e.target.value)
           }} />
-        <button className='bg-black text-white rounded px-4 py-3 text-2xl font-bold m-5'>Add Task</button>
+        <button className='bg-black text-white rounded px-4 py-3 text-2xl font-bold m-5 hover:bg-green-500 transition-all'>Add Task</button>
       </form>
       <hr />
-      <div className='p-8 bg-slate-200'>
+      <div className='p-3 '>
         {renderTask}
       </div>
     </>
